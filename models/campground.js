18649dd3a88ae05ba7316d65522e0e69a09e9cd1
Lixsr <osama142494@gmail.com>
2024-08-30 21:17:33 +0300

    Reviews Model Added

--- models/campground.js
+++ models/campground.js
@@ -5,7 +5,13 @@ const CampgourndSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 
