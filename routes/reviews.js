3ed9858620f5f1cffff468213947b84c43c9e824
Lixsr <osama142494@gmail.com>
2024-09-04 07:31:38 +0300

    Flash-success&error-Messages-added

--- routes/reviews.js
+++ routes/reviews.js
@@ -26,6 +26,7 @@ router.post("/", validateReview, catchAsync(async (req, res) => {
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash("success", "A New Review Has Been created!");
     res.redirect(`/campgrounds/${id}`);
 }));
 
@@ -33,6 +34,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "A Review Has Been Deleted!");
     res.redirect(`/campgrounds/${id}`);
 }));
 
