e0593d0fd0eb08cc83356123b9e6503790443eea
Lixsr <osama142494@gmail.com>
2024-09-07 20:19:56 +0300

    UploadImages(Multer)-added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
-const Campground = require("../models/campground");
 const campgrounds = require("../controllers/campgrounds");
-
-// storeURL will be used the store the return url after login
+// storeURL will be used to store the return url after login
 const { isLoggedIn, isAuthor, validateCampground, storeURL } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post( upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+    })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
