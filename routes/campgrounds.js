945fb2b7a8da30fcdc611fc35b906f4b85e88a12
Lixsr <osama142494@gmail.com>
2024-09-08 20:33:03 +0300

    cloudinary-added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
@@ -5,15 +10,17 @@ const campgrounds = require("../controllers/campgrounds");
 // storeURL will be used to store the return url after login
 const { isLoggedIn, isAuthor, validateCampground, storeURL } = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-    .post( upload.array('image'), (req, res) => {
-        console.log(req.body, req.files);
+    .post(upload.array('image'), (req, res) => {
+        // console.log(req.body, req.files); to be filled
+
     })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
